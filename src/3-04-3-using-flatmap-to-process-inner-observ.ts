cc2b62b783cd15daa146ef17c5935c49f1c37237
Apollo Tang <tang.apollo@gmail.com>
2018-09-20 08:09:14 +0800

    move rendering to the end of pipe line

---
+++ src/3-04-3-using-flatmap-to-process-inner-observ.ts
@@ -0,0 +1,80 @@
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap'; // this import flatMap, see:
+                                     // https://stackoverflow.com/questions/39441629/flatmap-missing-after-upgrading-to-rc-6-and-rxjs-beta-11
+
+const button = document.createElement('button');
+button.innerText = 'click me';
+document.body.appendChild(button);
+
+const outputDisplay = document.createElement('div');
+document.body.appendChild(outputDisplay);
+
+function load(url: string) {
+  return Observable.create( (observer:any) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", ()=>{
+      const movies = JSON.parse(xhr.responseText)
+      // renderMovies(movies) // <<<<< moved to end of pipeline
+
+      observer.next(movies)
+      observer.complete()
+    });
+
+    xhr.open('GET', url)
+    xhr.send();
+  })
+}
+
+function renderMovies(movies:any) {
+  movies.forEach( (m:any)=>{
+    const div = document.createElement('div');
+    div.innerText = m.title;
+    outputDisplay.appendChild(div)
+  })
+}
+
+let sourceClick = Observable
+  .fromEvent( button, 'click')
+
+// import {Observer} from 'rxjs/Observer';
+// class MyObjserver implements Observer<number> {
+//
+//   next(value: any) {
+//     console.log('xxxxxx value: ', JSON.stringify(value, null, 2))
+//     // xxxxxx value:  [
+//     //   {
+//     //     "title": "Star Wars"
+//     //   },
+//     //   {
+//     //     "title": "Star Trek"
+//     //   },
+//     //   {
+//     //     "title": "Starship Troopers"
+//     //   }
+//     // ]
+//
+//   }
+//
+//   error(e: any){
+//     console.error('error: ', e)
+//   }
+//
+//   complete(){
+//     console.log('done')
+//   }
+//
+// }
+
+sourceClick
+  .flatMap( e => load('assets/movies.json'))  // <<< we have flatten the nested subscription [desire-able]
+  // .subscribe( new MyObjserver())
+  .subscribe( renderMovies )
+
+
+// source.subscribe(
+//   (value: any) => console.log('value: ', value),
+//   (err: any) => console.error('something wrong occurred: ' + err),
+//   () => console.log('done')
+// )
